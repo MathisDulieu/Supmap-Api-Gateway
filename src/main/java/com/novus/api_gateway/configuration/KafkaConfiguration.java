04394df7145d07c021ffe1afc1c3059def31fb69
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-04-07 09:19:56 +0200

    [/] kafka config

--- src/main/java/com/novus/api_gateway/configuration/KafkaConfiguration.java
+++ src/main/java/com/novus/api_gateway/configuration/KafkaConfiguration.java
@@ -27,7 +27,7 @@ public class KafkaConfiguration {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, envConfiguration.getKafkaBootstrapServers());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.ACKS_CONFIG, "1");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.RETRIES_CONFIG, 3);
         props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 100);
         props.put(ProducerConfig.LINGER_MS_CONFIG, 10);
